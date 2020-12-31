4b14fd34ebde6fe824d67af8e2133c2d7d90bcae
Eugene Dedits <eugene.dedits@gmail.com>
2020-12-31 01:49:49 -0500

    mdified lsp config, mspyls for python

--- init.el
+++ init.el
@@ -34,19 +34,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" default))
  '(package-selected-packages
-   '(diredful dired+ org-roam magit yasnippet-snippets which-key use-package undo-tree try telephone-line smooth-scrolling smex smartparens org-bullets lsp-ui lsp-treemacs iedit flycheck expand-region eglot doom-themes doom-modeline counsel-projectile company ccls)))
+   '(lsp-python-ms pyvenv company-lsp company-box yasnippet-snippets which-key use-package undo-tree try telephone-line smooth-scrolling smex smartparens python-mode org-bullets magit lsp-ui lsp-treemacs lsp-jedi iedit ido-vertical-mode flycheck expand-region exec-path-from-shell eglot doom-themes doom-modeline diredful counsel-projectile company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
- '(org-block ((t nil)))
- '(org-block-begin-line ((t nil)))
- '(org-block-end-line ((t nil)))
  '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
