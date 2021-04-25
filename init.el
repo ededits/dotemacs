a3e4ea5beca2b5846b96a84579c83ff58448ce93
Eugene Dedits <eugene.dedits@gmail.com>
2021-04-24 22:04:16 -0400

    latex on, font, changes to LSP

--- init.el
+++ init.el
@@ -34,14 +34,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex  -synctex=1 --shell-escape")
+ '(TeX-PDF-mode t)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(TeX-view-program-list
+   '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %n %o %b")))
+ '(TeX-view-program-selection
+   '(((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Skim")
+     (output-html "xdg-open")))
+ '(custom-safe-themes
+   '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" default))
+ '(org-agenda-files '("~/Work/NYU/notes/NYU_main.org"))
  '(package-selected-packages
-   '(yafolding lsp-python-ms pyvenv company-lsp company-box yasnippet-snippets which-key use-package undo-tree try telephone-line smooth-scrolling smex smartparens python-mode org-bullets magit lsp-ui lsp-treemacs lsp-jedi iedit ido-vertical-mode flycheck expand-region exec-path-from-shell eglot doom-themes doom-modeline diredful counsel-projectile company)))
+   '(company-reftex company-math company-auctex auctex yasnippet-snippets yafolding which-key use-package undo-tree try telephone-line smooth-scrolling smex smartparens pyvenv python-mode org-bullets magit lsp-ui lsp-treemacs lsp-python-ms lsp-jedi iedit ido-vertical-mode flycheck expand-region exec-path-from-shell eglot doom-themes doom-modeline diredful counsel-projectile company-lsp company-box))
+ '(preview-gs-command "/usr/local/bin/gs")
+ '(preview-gs-options
+   '("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4") t)
+ '(pyvenv-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(org-block ((t (:extend nil))))
+ '(org-block-begin-line ((t (:extend nil))))
  '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
